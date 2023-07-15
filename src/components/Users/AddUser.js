6d29ce1ef0c30d712001fd3143a252a066b35b19
towficaziz <tapolly01@gmail.com>
2023-07-15 14:48:20 +0200

    Adding custom wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,27 +2,27 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
-import UsersList from "./UsersList";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  const [error, setError]=useState();
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
-        title: 'Invalid input',
-        message: 'Please enter a valid name and age (non-empty values).'
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
     if (+enteredAge < 10) {
       setError({
-        title: 'Invalid age',
-        message: 'Please enter a valid age (> 10).'
+        title: "Invalid age",
+        message: "Please enter a valid age (> 10).",
       });
       return;
     }
@@ -39,13 +39,19 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
-  const errorHandler = () =>{
+  const errorHandler = () => {
     setError(null);
   };
 
   return (
-    <div>
-      {error &&  <ErrorModal title={error.title} message={error.message}  onConfirm={errorHandler} />}
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -66,7 +72,7 @@ const AddUser = (props) => {
           <Button type="submit"> Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
